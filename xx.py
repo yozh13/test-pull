b165175390f0209438926bd5900871f98fbc999a
yaozhen <1308378024@qq.com>
2024-08-15 01:01:52 +0800

    test

--- xx.py
+++ xx.py
@@ -1,2 +1,17 @@
-# master_tester 分支做了修改
-print('a')
+# mrster_tester 分支做了修改
+from my_module.test import test
+
+print('r')
+print('r')
+print('r')
+print('r')
+print('r')
+a = 1
+
+
+def test_1():
+    print('rr')
+
+
+test_1()
+test()
